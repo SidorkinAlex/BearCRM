3af0bc9c933ffdfa3fdfd1342c5a27d52807cac8
SidorkinAlex <palach.orl@mail.ru>
2022-04-30 10:13:30 +0300

    added hook from adding board buttom in menu

--- custom/Extension/application/Ext/LogicHooks/AOW_WorkFlow_Hook.php
+++ custom/Extension/application/Ext/LogicHooks/AOW_WorkFlow_Hook.php
@@ -29,3 +29,13 @@ if (!isset($hook_array['after_save']) || !is_array($hook_array['after_save'])) {
     $hook_array['after_save'] = array();
 }
 $hook_array['after_save'][] = Array(99, 'AOW_Workflow', 'modules/AOW_WorkFlow/AOW_WorkFlow.php','AOW_WorkFlow', 'run_bean_flows');
+
+if (!isset($hook_array['after_ui_footer']) || !is_array($hook_array['after_ui_footer'])) {
+    $hook_array['after_ui_footer'] = array();
+}
+$hook_array['after_ui_footer'][] = array(
+    100,
+    'adding js file to page',
+    'modules/BOARD/hooks/js_add_hook.php',
+    'Js_Add_Hook',
+    'after_ui_frame_method');
