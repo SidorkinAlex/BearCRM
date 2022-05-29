b74e36d191041c6fc13de706e39e17273e13dd4a
SidorkinAlex <palach.orl@mail.ru>
2022-05-29 13:08:02 +0300

    addeed fields from ModuleLoader class

--- modules/ModuleLoader/ModuleLoader.php
+++ modules/ModuleLoader/ModuleLoader.php
@@ -1,4 +1,28 @@
 <?php
 class ModuleLoader extends SugarBean {
+    public $field_name_map = array();
+    // Stored fields
+    public $date_entered;
+    public $date_modified;
+    public $modified_user_id;
+    public $assigned_user_id;
+    public $annual_revenue;
+    public $id;
+    public $name;
 
+
+    public $created_by;
+    public $created_by_name;
+    public $modified_by_name;
+
+    // These are for related fields
+
+    public $module_dir = 'ModuleLoader';
+    public $emailAddress;
+
+
+    public $table_name = "moduleloader";
+    public $object_name = "ModuleLoader";
+    public $importable = false;
+    public $new_schema = true;
 }
